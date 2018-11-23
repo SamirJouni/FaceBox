3375c1951005316c22f07253350c2a076e313872
F1r3byte <f1r3byte@yandex.com>
2018-11-23 23:13:49 +0100

    added navigation into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Navigation />
+			</div>
+		);
+	}
 }
 
 export default App;
