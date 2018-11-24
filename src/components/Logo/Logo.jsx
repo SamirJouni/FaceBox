9810d0cb6dcf4099052c9639c430e1460f2db3d6
F1r3byte <f1r3byte@yandex.com>
2018-11-24 23:24:02 +0000

    added logo and styled it

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Tilt from "react-tilt";
+import './Logo.css';
+import Icon from './Icon.png';
 
 const Logo = () => {
 	return (
 		<div className="ma4 mt0">
 			<Tilt
-				className="Tilt br2 shadow-2"
-				options={{ max: 25 }}
+				className="Tilt br2 shadow-2 flex items-center justify-center"
+				options={{ max: 55 }}
 				style={{ height: 150, width: 150 }}
 			>
-				<div className="Tilt-inner"> 👽 </div>
+				<div className="Tilt-inner flex"><img src={Icon} alt="Icon of the website."/></div>
 			</Tilt>
 		</div>
 	);
