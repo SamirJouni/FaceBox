a4c14cf0bc6dd27d5f4687e79f2af1462bf8f160
F1r3byte <f1r3byte@yandex.com>
2018-11-27 23:38:06 +0100

    added code from api

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,14 @@ class App extends Component {
 
 	onSubmit = e => {
 		e.preventDefault();
+		app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 	}
 
 	render() {
