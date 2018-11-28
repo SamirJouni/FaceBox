1798ede4156701a3ed12130dba351aa18987bbae
F1r3byte <f1r3byte@yandex.com>
2018-11-28 23:26:25 +0000

    got face box working

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -3,15 +3,25 @@ import "./FaceRecognition.css";
 
 const FaceRecognition = props => {
 	return (
-		<div className="flex justify-center items-center mt4 ma">
-			<img
-				id="providedImage"
-				src={props.linkToImage}
-				alt=""
-				width="500px"
-				height="auto"
-			/>
-			<div className="bounding-box"></div>
+		<div className="flex justify-center items-center center mt4 ma">
+			<div className="relative">
+				<img
+					id="providedImage"
+					src={props.linkToImage}
+					alt=""
+					width="500px"
+					height="auto"
+				/>
+				<div
+					className="bounding-box"
+					style={{
+						top: props.boundingBox.top,
+						left: props.boundingBox.left,
+						right: props.boundingBox.right,
+						bottom: props.boundingBox.bottom
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
