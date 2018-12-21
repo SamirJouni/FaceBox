ab11fa2654fc90c30c8132425afa192eb605ad53
F1r3byte <f1r3byte@yandex.com>
2018-12-20 15:26:51 -1200

    fixed a bug

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -13,7 +13,7 @@ const SignUp = props => {
 							</label>
 							<input
 								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-								type="name"
+								type="text"
 								name="name"
 								id="name"
 							/>
