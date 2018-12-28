7bab9d80437bf20f8303f1d73affb709848e01d9
F1r3byte <f1r3byte@yandex.com>
2018-12-27 22:30:54 -1200

    ranking system now working

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
 	return (
 		<div className="flex justify-center items-center flex-column">
-			<div>{`${name} , your current rank is...`}</div>
+			<div>{name} , your current rank is...</div>
 			<div className="white f1 ">{entries}</div>
 		</div>
 	);
